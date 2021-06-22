f857cef01abafc8caff1cf88a42c7612a4349be9
rxliuli <rxliuli@gmail.com>
2021-06-22 19:19:51 +0800

    Updates

---
+++ joplin-batch-web/assets/mdParser.f93b0f79.js
@@ -0,0 +1,1 @@
+import{d as e,f as s,g as r,h as a}from"./vendor.40071de5.js";const t=e().use(s).use(r).use(a,{bullet:"-",fences:!0,incrementListMarker:!1});export{t as m};
---
+++ joplin-batch-web/assets/parseInternalLink.0946e94b.js
@@ -0,0 +1,1 @@
+import{e as t,c as r}from"./vendor.40071de5.js";import{m as e}from"./mdParser.f93b0f79.js";t((function(t){const i=[];return r(e.parse(t),(t=>{var r;if("link"!==t.type&&"image"!==t.type)return;const e=t;e.url.startsWith(":/")&&i.push({id:e.url.slice(2),title:"link"===e.type?null!=(r=e.title)?r:e.children[0].value:e.alt})})),i}));
